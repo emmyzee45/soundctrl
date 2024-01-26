fe4b9f7dc7c22be4bb53b738b2c1fa682386b123
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2024-01-26 15:35:55 +0100

    booking api test

--- controllers/booking.js
+++ controllers/booking.js
@@ -18,11 +18,34 @@ export const createTicket = async (req, res, next) => {
   }
 };
 
+// update tickets by artist
+export const updateTickets = async (req, res, next) => {
+  try {
+    const tickets = await Ticket.findByAndUpdate(req.params.id, req.body, {new: true});
+    if(!tickets) return next(createError(404, "Not found!"));
+
+    res.status(200).send(tickets);
+  } catch (err) {
+    next(err);
+  }
+};
+
 // get tickets by artist
+export const getTicketsByArtist = async (req, res, next) => {
+  try {
+    const tickets = await Ticket.find({ artistId: req.params.id });
+    if(!tickets) return next(createError(404, "Not found!"))
+    res.status(200).send(tickets);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// get all tickets
 export const getTickets = async (req, res, next) => {
   try {
-    const Tickets = await Ticket.find({ artistId: req.params.id });
-    res.status(200).send(Tickets);
+    const tickets = await Ticket.find();
+    res.status(200).send(tickets);
   } catch (err) {
     next(err);
   }
@@ -31,8 +54,8 @@ export const getTickets = async (req, res, next) => {
 // delete ticket by artist
 export const deleteTicket = async (req, res, next) => {
   try {
-    const Tickets = await Ticket.findByIdAndDelete(req.params.id);
-    res.status(200).send(Tickets);
+    const tickets = await Ticket.findByIdAndDelete(req.params.id);
+    res.status(200).send(tickets);
   } catch (err) {
     next(err);
   }
