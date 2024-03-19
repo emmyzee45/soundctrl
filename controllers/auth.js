a01dd61c9004e34df6ee8222f994399330c5d188
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-03-19 05:54:34 +0100

    eror setting

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,14 +25,14 @@ export const register = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
-  console.log(req.body)
   try {
     const user = await User.findOne({ email: req.body.email });
-
+    
     if (!user) return next(createError(404, "User not found!"));
-
+    console.log(user)
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect)
+    console.log(isCorrect)
       return next(createError(400, "Wrong password or email!"));
 
     const token = jwt.sign(
