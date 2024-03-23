9c171c3100abfc60cc1f7543f87819ad0f4797d0
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-03-23 10:37:42 +0100

    url change

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const connect = async () => {
 };
 // { origin: "http://18.234.121.45", credentials: true }
 // ec2-52-87-176-35.compute-1.amazonaws.com
-// ec2-54-234-208-192.compute-1.amazonaws.com
-app.use(cors({ origin: "http://ec2-54-234-208-192.compute-1.amazonaws.com", credentials: true }));
+// ec2-35-174-136-54.compute-1.amazonaws.com
+app.use(cors({ origin: "http://ec2-35-174-136-54.compute-1.amazonaws.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
