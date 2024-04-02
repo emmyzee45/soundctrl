3f1ebca04bd26f1914fe4947929e5e963f0db748
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-04-02 13:03:30 +0100

    json web token

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -5,7 +5,6 @@ export const verifyToken = (req, res, next) => {
   // const authHeader = req.headers["authorization"];
   // const token = authHeader && authHeader.split(" ")[1];
   const token = req.cookies.accessToken;
-
   if (!token) return next(createError(401,"You are not authenticated!"))
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
