17cff5464e522e6956141ae7cb142b0fc0431151
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-04-02 14:36:25 +0100

    cookie parser

--- server.js
+++ server.js
@@ -25,9 +25,18 @@ const connect = async () => {
     console.log(error);
   }
 };
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
 // { origin: "http://18.234.121.45", credentials: true }
 // ["http://ec2-107-23-210-120.compute-1.amazonaws.com", "http://ec2-100-26-240-163.compute-1.amazonaws.com"]
-app.use(cors({ origin:"http://ec2-100-26-240-163.compute-1.amazonaws.com", credentials: true }));
+app.use(cors({ origin: 
+  [
+  "http://ec2-107-23-210-120.compute-1.amazonaws.com", 
+  "http://ec2-100-26-240-163.compute-1.amazonaws.com",
+  "http://localhost:3000"
+], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
