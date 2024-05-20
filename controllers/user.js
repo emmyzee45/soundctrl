ef2177bf67f16ef1b41b727b8c719864b0e7ed15
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-05-20 21:25:08 +0100

    testing stripe

--- controllers/user.js
+++ controllers/user.js
@@ -20,7 +20,7 @@ export const getSingleUser = async (req, res, next) => {
 
 export const getAllArtist = async (req, res, next) => {
   try {
-    const user = await User.find({isArtist: true, onboarding_complete: true });
+    const user = await User.find({isArtist: true });
     res.status(200).json(user);
   }catch(err) {
     next(err)
